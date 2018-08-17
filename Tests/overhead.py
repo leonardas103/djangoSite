3212aeb17cb2809b0b653f524e9ef15d6c4ce8c1
leonardas103 <wingdragon_9@hotmail.com>
2018-08-17 02:54:24 +0200

    tested shared memory

--- Tests/overhead.py
+++ Tests/overhead.py
@@ -6,36 +6,36 @@ import time
 timing = []
 
 
-def overhead_worker_joblib(image):
+def worker_joblib(image):
 	return image
 
-def overhead_joblib(image):
+def joblib(image):
 	image_tiles = np.array_split(image, num_cores, axis=0)
-	return_value = Parallel(n_jobs=num_cores)(delayed(overhead_worker_joblib)(image_tiles[i],) for i in range(num_cores))
+	return_value = Parallel(n_jobs=num_cores)(delayed(worker_joblib)(image_tiles[i],) for i in range(num_cores))
 	image = np.vstack(return_value)
 	return image
 
-def overhead_worker_pool(args):
+def worker_pool(args):
 	image, dummy_var = args
 	return image
 
 
-def overhead_pool(image):
+def pool(image):
 	image_tiles = np.array_split(image, num_cores, axis=0)
 	dummy_var = 0
 	args = zip(image_tiles, [dummy_var]*num_cores,)
 	with mp.Pool(processes=num_cores) as p:
-		image = p.map(overhead_worker_pool, args)
-		# image = p.map_async(overhead_worker_pool, args).get()
+		image = p.map(worker_pool, args)
+		# image = p.map_async(worker_pool, args).get()
 	return np.vstack(image)
 
-def overhead_worker_process(queue, image):
+def worker_process(queue, image):
 	queue.put(image)
 
-def overhead_process(image):
+def process(image):
 	image_tiles = np.array_split(image, num_cores, axis=0)
 	queues = [mp.SimpleQueue() for i in range(num_cores)]
-	jobs = [mp.Process(target=overhead_worker_process, args=[queues[i], image_tiles[i]]) for i in range(num_cores)]
+	jobs = [mp.Process(target=worker_process, args=[queues[i], image_tiles[i]]) for i in range(num_cores)]
 	
 	for job in jobs: job.start()
 	return_value = [queue.get() for queue in queues]
@@ -45,14 +45,14 @@ def overhead_process(image):
 	return image
 
 
-def overhead_worker_process_pipe(pipe, image):
+def worker_process_pipe(pipe, image):
 	pipe.send(image)
 	pipe.close()
 
-def overhead_process_pipe(image):
+def process_pipe(image):
 	image_tiles = np.array_split(image, num_cores, axis=0)
 	pipes = [mp.Pipe(duplex=False) for _ in range(num_cores)]
-	jobs = [mp.Process(target=overhead_worker_process_pipe, args=[pipes[i][1], image_tiles[i]]) for i in range(num_cores)]
+	jobs = [mp.Process(target=worker_process_pipe, args=[pipes[i][1], image_tiles[i]]) for i in range(num_cores)]
 	
 	for job in jobs: job.start()
 	pipes_out = [pipe[0].recv() for pipe in pipes]
@@ -61,6 +61,20 @@ def overhead_process_pipe(image):
 	image = np.vstack(pipes_out)
 	return image
 
+def worker_process_manager(image, shared_memory, idx):
+	shared_memory.append(image)
+
+def process_manager(image):
+	image_tiles = np.array_split(image, num_cores, axis=0)
+	manager = mp.Manager()
+	shared_memory = manager.list()
+	jobs = [mp.Process(target=worker_process_manager, args=[image_tiles[i], shared_memory, i]) for i in range(num_cores)]
+	
+	for job in jobs: job.start()
+	for job in jobs: job.join()
+
+	return np.vstack(shared_memory[:])
+
 def control(image):
 	image_tiles = np.array_split(image, num_cores, axis=0)
 	s = np.vstack(image_tiles)
@@ -68,7 +82,7 @@ def control(image):
 
 def time_function(func, images):
 	results = []
-	num_tests = 10
+	num_tests = 5
 	for img in images:
 		times = []
 		for _ in range(num_tests):
@@ -86,15 +100,16 @@ num_cores = mp.cpu_count()
 
 def main():
 	images = []
-	images.append(np.matrix(np.random.randint(0,255, size=(1000, 1001))))
+	images.append(np.matrix(np.random.randint(0,255, size=(1000, 1000))))
 	images.append(np.matrix(np.random.randint(0,255, size=(2000, 2000))))
 	images.append(np.matrix(np.random.randint(0,255, size=(4000, 4000))))
 
 	A = time_function('control', images)
-	B = time_function('overhead_process_pipe', images)
-	C = time_function('overhead_process', images)
-	D = time_function('overhead_joblib', images)
-	E = time_function('overhead_pool', images)
+	B = time_function('process_pipe', images)
+	C = time_function('process', images)
+	D = time_function('joblib', images)
+	E = time_function('pool', images)
+	F = time_function('process_manager', images)
 	
 
 	for i in timing:
